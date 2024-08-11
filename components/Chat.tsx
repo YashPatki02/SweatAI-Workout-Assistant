a4637435baec311ed03c44548b81e47ee880b773
Yash Patki <yashom.patki@gmail.com>
2024-08-11 00:33:49 -0700

    Add ScrollView

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,16 +1,36 @@
 import { Input } from "@/components/ui/input";
 import React from "react";
 import { Button } from "./ui/button";
+import { ScrollArea } from "./ui/scroll-area";
 
 const Chat = () => {
     return (
-        <div className="relative flex flex-col flex-1 p-4 px-6 w-full h-screen">
+        <div className="relative flex flex-col flex-1 p-4 px-6">
             <h1 className="text-xl font-semibold mb-4">Chat</h1>
 
             {/* Main content area */}
-            <div className="flex-grow">
-                <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
-            </div>
+            <ScrollArea className="h-[70vh] mb-4">
+                <div className="flex flex-col gap-2">
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                    <h3>UHISUHFIUHISUHISUHDISUHFIUHFIUHFIUH</h3>
+                </div>
+            </ScrollArea>
 
             {/* Input area */}
             <div className="absolute bottom-0 right-4 left-4 flex flex-row gap-2">
